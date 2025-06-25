14ce0be462568c73dfdb7c45f6fa5e566c22ac2a
Nicolás Vargas <elias.nicolas.vargas@gmail.com>
2025-06-25 11:01:14 -0300

    Correccion WArnins

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -19,12 +19,9 @@ export default function Contacto() {
       <h3>Teléfono: 294 467-7439 </h3>
 
       <h3>
-        Mail:{" "}
-        <a>
-          Huemuleshockeyclub@gmail.com
-        </a>{" "}
-        - Seguinos{" "}
+        Mail: Huemuleshockeyclub@gmail.com - Seguinos{" "}
         <a
+          className="instagram-link"
           href="https://www.instagram.com/huemuleshcbariloche/"
           target="_blank"
           rel="noopener noreferrer"
@@ -36,6 +33,7 @@ export default function Contacto() {
       <div className='mapas-container'>
         <div className="mapa_1">
           <iframe
+            title="Ubicación ESRN 99"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629.9999145719903!2d-71.30866983430235!3d-41.14753635147278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x961a7b410070b1e5%3A0xa7d1445c2c90f21a!2sESRN%20N%C2%B0%2099!5e1!3m2!1ses-419!2sar!4v1750252949167!5m2!1ses-419!2sar"
             width="100%"
             height="200"
@@ -48,18 +46,18 @@ export default function Contacto() {
 
         <div className='mapa_2'>
           <iframe 
+            title="Ubicación New Lawn"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d891.0378629250425!2d-71.32088537149967!3d-41.14139527734489!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x961a7b10dd2e89bb%3A0xe053378f9b056aac!2sNew%20Lawn!5e1!3m2!1ses-419!2sar!4v1750253787470!5m2!1ses-419!2sar" 
             width="100%" 
             height="200" 
             style={{ border: 0 }} 
-            allowfullscreen="" 
+            allowFullScreen="" 
             loading="lazy" 
-            referrerpolicy="no-referrer-when-downgrade">
-          </iframe>
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
-
       </div>
-      
+
 
 
     </div>
