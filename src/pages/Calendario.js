8b6198702f16437d816d5913617c653f188a28c9
Nicolás Vargas <elias.nicolas.vargas@gmail.com>
2025-09-19 10:00:16 -0300

    CARGAR HORARIOS

--- src/pages/Calendario.js
+++ src/pages/Calendario.js
@@ -7,29 +7,19 @@ export default function Calendario() {
       <h1>Próximas fechas</h1> 
 
       <div className="dia-partidos">
-        <h2>Sábado 23 de Agosto</h2>
+        <h2>Sábado 30 Septiembre</h2>
         <h3>Damas</h3>
-        <p>Huemules B vs Bolson B</p>
-        <p>18:00 hs</p>
-        <h3 className="ubicacion">New Lawn</h3>
-        <h3>Caballeros</h3>
-        <p>Huemules vs Estudiantes</p>
+        <p>Huemules B vs Huemules A</p>
         <p>14:00 hs</p>
-        <p>Huemules vs Esc. Municipal A</p>
-        <p>20:00 hs</p>
-        <h3 className="ubicacion">Polideportivo Dina Huapi</h3>
+        <p>El Bolson A vs Huemules B</p>
+         <p>18:00 hs</p>
+        <p>El Bolson B vs Huemules A</p>
+         <p>19:00 hs</p>
+        <h3 className="ubicacion">New Lawn</h3>
       </div>
+  
 
-      <div className="dia-partidos">
-        <h2>Domingo 24 de Agosto</h2>
-        <h3>Caballeros</h3>
-        <p>Huemules vs Hualas</p>
-        <p>14:30 hs</p>
-        <h3>Damas</h3>
-        <p>Huemules A vs Lacar</p>
-        <p>15:30 hs</p>
-        <h3 className="ubicacion">Polideportivo Dina Huapi</h3>
-      </div>
+      
     </div>
   );
 }
